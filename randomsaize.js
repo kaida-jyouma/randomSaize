dd2db949dd76d7c749c229b2a65063d378835678
kouji <53356184+kaida-jyouma@users.noreply.github.com>
2024-08-12 13:14:23 +0900

    Add files via upload

---
+++ randomsaize.js
@@ -0,0 +1,129 @@
+var version = 1; // last update: 20240812
+var menuIdentNum = "2024.06";
+var sel = 0;
+var menuList = ["A", "B", "C"];
+var grandMenu;
+var randlist = [];
+var lst = localStorage;
+if (lst.getItem("#sizry") === null || lst.getItem("#sizry") === "" || lst.getItem("#sizry") === undefined){
+	lst.setItem("#sizry", '{"info":{"version":' + version + ',"menuIdent":' + menuIdentNum + '},"saveData":{"menu":"A","cat":6},"latest":{"1":"","2":"","3":"","4":"","5":""}}');
+}else if (JSON.parse(lst.getItem("#sizry"))["info"]["version"] < version){
+	var sizry_Storage = JSON.parse(lst.getItem("#sizry"));
+	sizry_Storage["info"]["version"] = version;
+	sizry_Storage["info"]["menuIdent"] = menuIdentNum;
+	lst.setItem("#sizry", JSON.stringify(sizry_Storage));
+}
+function selectMenu(){
+	sel = menuList.indexOf(document.getElementById("menuNum").value) + 1;
+	console.log(sel);
+
+	if (sel === 1){
+		grandMenu = menuA;
+	}else if (sel === 2){
+		grandMenu = menuB;
+	}else if (sel === 3){
+		grandMenu = menuC;
+	}else{
+		grandMenu = menuA;
+		sel = 1;
+	}
+	document.getElementById("main").innerHTML = '<p class="msg-info">選択範囲設定</p><p class="msg-alert">出現するメニューの範囲を選択してください</p><input type="button" class="selector" id="sel2" onclick="setRange(1)" value="サラダ・前菜・スープ"><br><input type="button" class="selector" id="sel3" onclick="setRange(2)" value="サイド"><br><input type="button" class="selector" id="sel4" onclick="setRange(3)" value="メイン"><br><input type="button" class="selector" id="sel5" onclick="setRange(4)" value="デザート"><br><input type="button" class="selector" id="sel6" onclick="setRange(5)" value="全メニュー ( ｱﾙｺｰﾙは除く )"><br><input type="button" class="selector" id="sel7" onclick="setRange(6)" value="全メニュー"><br><input type="button" class="selector" id="sel8" onclick="returnTop()" value="はじめに戻る"><br>';
+}
+function setRange(num){
+	randlist = [];
+	if (num === 1){
+		var selected = "サラダ・前菜・スープ";
+	}else if (num === 2){
+		var selected = "サイド";
+	}else if (num === 3){
+		var selected = "メイン";
+	}else if (num === 4){
+		var selected = "デザート";
+	}else if (num === 5){
+		var selected = "全メニュー ( ｱﾙｺｰﾙは除く )";
+	}else{
+		var selected = "全メニュー";
+	}
+
+	var keyList = Object.keys(grandMenu);
+	for (i=0;i<keyList.length;i++){
+		var cat = grandMenu[keyList[i]]["category"];
+		if (cat === "salad" || cat === "soup" || cat === "appetizer"){
+			if (num === 1 || num === 5 || num === 6){
+				randlist.push(keyList[i]);
+			}
+		}else if (cat === "bread" || cat === "side menu"){
+			if (num === 2 || num === 5 || num === 6){
+				randlist.push(keyList[i]);
+			}
+		}else if (cat === "drink bar" || cat == "kid's menu" || cat === "topping"){
+			if (num === 5 || num === 6){
+				randlist.push(keyList[i]);
+			}
+		}else if (cat === "alcohol" || cat === "alcohol (bottle)"){
+			if (num === 6){
+				randlist.push(keyList[i]);
+			}
+		}else if (cat === "pizza" || cat === "doria & gratin" || cat === "pasta" || cat === "hamburg" || cat === "chicken" || cat === "lamb"){
+			if (num === 3 || num === 5 || num === 6){
+				randlist.push(keyList[i]);
+			}
+		}else if (cat === "dessert"){
+			if (num === 4 || num === 5 || num === 6){
+				randlist.push(keyList[i]);
+			}
+		}
+	}
+
+	if (sel === 1){
+		var usingMenu = "2024.06 A";
+	}else if (sel === 2){
+		var usingMenu = "2024.06 B";
+	}else if (sel === 3){
+		var usingMenu = "2024.06 C";
+	}
+	
+	document.getElementById("main").innerHTML = '<p class="msg-info">条件確認</p><p class="msg-alert">以下の条件で抽選されます<br>よろしいですか？</p><p class="msg-info">' + selected + '</p><p class="msg-alert">使用メニュー: ' + usingMenu + '</p><br><input type="button" class="selector" id="sel9" onclick="choiceMenu(' + num + ')" value="サイコロを振る"><br><input type="button" class="selector" id="sel10" onclick="returnSelection()" value="戻る"><br><input type="button" class="selector" id="sel11" onclick="returnTop()" value="はじめに戻る">';
+}
+function returnTop(){
+	sel = 0;
+	menuList = ["A", "B", "C"];
+	grandMenu = null;
+	randlist = [];
+	console.log("return-Top");
+	document.getElementById("main").innerHTML = '<p class="msg-info">初期設定</p><p class="msg-alert">メニュー裏面右下に印字された識別記号を選択してください</p><select name="menuNum" id="menuNum" autocomplete="on"><option value="A">2024.06 A</option> <!--全国版--> <option value="B">2024.06 B</option> <!--北海道/東京/神奈川--> <option value="C">2024.06 C</option> <!--埼玉/東京/神奈川--> </select><br><input type="button" class="selector" id="sel1" onclick="selectMenu()" value="はじめる">';
+}
+function returnSelection(){
+	randlist = [];
+	console.log("return-Selection");
+	document.getElementById("main").innerHTML = '<p class="msg-info">選択範囲設定</p><p class="msg-alert">出現するメニューの範囲を選択してください</p><input type="button" class="selector" id="sel2" onclick="setRange(1)" value="サラダ・前菜・スープ"><br><input type="button" class="selector" id="sel3" onclick="setRange(2)" value="サイド"><br><input type="button" class="selector" id="sel4" onclick="setRange(3)" value="メイン"><br><input type="button" class="selector" id="sel5" onclick="setRange(4)" value="デザート"><br><input type="button" class="selector" id="sel6" onclick="setRange(5)" value="全メニュー ( ｱﾙｺｰﾙは除く )"><br><input type="button" class="selector" id="sel7" onclick="setRange(6)" value="全メニュー"><br><input type="button" class="selector" id="sel8" onclick="returnTop()" value="はじめに戻る"><br>';
+}
+function choiceMenu(selNum){
+	var sizry_nowStorage = JSON.parse(lst.getItem("#sizry"));
+	if (sel === 1){
+		sizry_nowStorage["saveData"]["menu"] = "A";
+	}else if (sel === 2){
+		sizry_nowStorage["saveData"]["menu"] = "B";
+	}else if (sel === 3){
+		sizry_nowStorage["saveData"]["menu"] = "C";
+	}else{
+		sizry_nowStorage["saveData"]["menu"] = "A";
+	}
+	sizry_nowStorage["saveData"]["cat"] = selNum;
+	
+	
+	// format = Math.floor( Math.random() * (max + 1 - min) ) + min;
+    var rnum = Math.floor( Math.random() * (randlist.length + 1 - 0) ) + 0;
+	var hitMenuNum = randlist[rnum];
+	var hitMenuName = grandMenu[hitMenuNum]["name"];
+
+	document.getElementById('main').innerHTML = '<p class="msg-info">抽選結果</p><p class="msg-alert">抽選されたメニュー番号は以下の通りです:</p><p class="msg-info" id="num-disp"></p><p class="msg-alert" id="menu-disp" style="color: #f9d39d;"></p><br><input type="button" class="selector" id="sel12" onclick="" value="もう一度"><br><input type="button" class="selector" id="sel13" onclick="returnSelection()" value="条件変更"><br><input type="button" class="selector" id="sel14" onclick="returnTop()" value="はじめに戻る">'
+	document.getElementById('num-disp').innerHTML = hitMenuNum;
+	document.getElementById('menu-disp').style.color = "#f9d39d";
+	document.getElementById('menu-disp').innerHTML = hitMenuName;
+	lst.setItem("#sizry", JSON.stringify(sizry_nowStorage));
+
+}
+function reChoice(){
+
+}
---
+++ randomsaizeMenuListAll.js
@@ -0,0 +1,522 @@
+var menuA = {
+	"0000": {
+		"name": "update / area info",
+		"price": 2024061,
+		"category": "2024.06 A"
+	},
+	"1202": {
+		"name": "Shrimp Salad",
+		"price": 350,
+		"category": "salad"
+	},
+	"1209": {
+		"name": "Chicken Salad",
+		"price": 350,
+		"category": "salad"
+	},
+	"1205": {
+		"name": "Seaweed Salad",
+		"price": 350,
+		"category": "salad"
+	},
+	"1208": {
+		"name": "Green Salad",
+		"price": 350,
+		"category": "salad"
+	},
+	"1207": {
+		"name": "Green Salad with Buffalo Mozzarella Cheese",
+		"price": 400,
+		"category": "salad"
+	},
+	"1301": {
+		"name": "Corn Cream Soup",
+		"price": 150,
+		"category": "soup"
+	},
+	"1307": {
+		"name": "Onion Soup with Bread & Cheese",
+		"price": 300,
+		"category": "soup"
+	},
+	"1305": {
+		"name": "Minestrone",
+		"price": 300,
+		"category": "soup"
+	},
+	"3108": {
+		"name": "Mini Ficelle",
+		"price": 150,
+		"category": "bread"
+	},
+	"3109": {
+		"name": "Garlic Toast",
+		"price": 200,
+		"category": "bread"
+	},
+	"3106": {
+		"name": "Petit Focaccia",
+		"price": 150,
+		"category": "bread"
+	},
+	"3104": {
+		"name": "Petit Focaccia with Cinnamon Sugar",
+		"price": 200,
+		"category": "bread"
+	},
+	"5101": {
+		"name": "Drink Bar (Set Menu)",
+		"price": 200,
+		"category": "drink bar"
+	},
+	"5102": {
+		"name": "Drink Bar (Kid's Menu)",
+		"price": 100,
+		"category": "kid's menu"
+	},
+	"5103": {
+		"name":"Drink Bar (Single Menu)",
+		"price": 300,
+		"category": "drink bar"
+	},
+	"1423": {
+		"name": "Jamon Serrano & Buffalo Mozzarella Cheese",
+		"price": 500,
+		"category": "appetizer"
+	},
+	"1422": {
+		"name": "Jamon Serrano",
+		"price": 320,
+		"category": "appetizer"
+	},
+	"1414": {
+		"name": "Buffalo Mozzarella Cheese & Tomato",
+		"price": 430,
+		"category": "appetizer"
+	},
+	"1413": {
+		"name": "Shredded Carrot",
+		"price": 200,
+		"category": "appetizer"
+	},
+	"1406": {
+		"name": "Shrimp with 'SAIZERIYA' Dressing",
+		"price": 280,
+		"category": "appetizer"
+	},
+	"1408": {
+		"name": "Chicken with Flavored Vegetable Sauce",
+		"price": 280,
+		"category": "appetizer"
+	},
+	"3401": {
+		"name": "Glass Wine (Red/120ml)",
+		"price": 100,
+		"category": "alcohol"
+	},
+	"3402": {
+		"name": "Glass Wine (White/120ml)",
+		"price": 100,
+		"category": "alcohol"
+	},
+	"3403": {
+		"name": "Decanter Wine (Red/250ml)",
+		"price": 250,
+		"category": "alcohol"
+	},
+	"3404": {
+		"name": "Decanter Wine (White/250ml)",
+		"price": 250,
+		"category": "alcohol"
+	},
+	"3405": {
+		"name": "Decanter Wine (Red/500ml)",
+		"price": 400,
+		"category": "alcohol"
+	},
+	"3406": {
+		"name": "Decanter Wine (White/500ml)",
+		"price": 400,
+		"category": "alcohol"
+	},
+	"3407": {
+		"name": "Magnum Wine (Red/1500ml)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3408": {
+		"name": "Magnum Wine (White/1500ml)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3301": {
+		"name": "Beer (mug)",
+		"price": 400,
+		"category": "alcohol"
+	},
+	"3302": {
+		"name": "Beer (glass)",
+		"price": 300,
+		"category": "alcohol"
+	},
+	"3304": {
+		"name": "Hyo-ketsu (KIRIN/Lemon-sour)",
+		"price": 350,
+		"category": "alcohol"
+	},
+	"3303": {
+		"name": "Dry-ZERO (ASAHI/Non-alcohol)",
+		"price": 250,
+		"category": "alcohol (bottle)"
+	},
+	"3413": {
+		"name": "Don Raffaele (White/Sparkling)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3412": {
+		"name": "Lambrusco Rose (Rose/Sparkling)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3414": {
+		"name": "Lambrusco Secco (Red/Sparkling)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3415": {
+		"name": "Verdicchio (White)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3416": {
+		"name": "Chianti (Red)",
+		"price": 1100,
+		"category": "alcohol (bottle)"
+	},
+	"3419": {
+		"name": "Chianti Rufina Riserva (Red)",
+		"price": 2200,
+		"category": "alcohol (bottle)"
+	},
+	"1405": {
+		"name": "Grilled Escargots",
+		"price": 400,
+		"category": "appetizer"
+	},
+	"1401": {
+		"name": "Grilled Spicy Chicken",
+		"price": 300,
+		"category": "appetizer"
+	},
+	"1404": {
+		"name": "Fried Shrimp",
+		"price": 300,
+		"category": "appetizer"
+	},
+	"1415": {
+		"name": "Grilled Potato",
+		"price": 280,
+		"category": "appetizer"
+	},
+	"1407": {
+		"name": "Grilled Spicy Sausage",
+		"price": 400,
+		"category": "appetizer"
+	},
+	"1410": {
+		"name": "Grilled Green Mussels with Vegetable Salsa",
+		"price": 400,
+		"category": "appetizer"
+	},
+	"1402": {
+		"name": "Grilled Skewered Lamb",
+		"price": 400,
+		"category": "appetizer"
+	},
+	"1452": {
+		"name": "Grilled Skewered Lamb (Double Size)",
+		"price": 800,
+		"category": "appetizer"
+	},
+	"1425": {
+		"name": "Green Peas with Gran Moravia Cheese & Egg",
+		"price": 200,
+		"category": "appetizer"
+	},
+	"1403": {
+		"name": "Sauteed Spinach",
+		"price": 200,
+		"category": "appetizer"
+	},
+	"1411": {
+		"name": "Asparagus with Gran Moravia Cheese",
+		"price": 300,
+		"category": "appetizer"
+	},
+	"2203": {
+		"name": "PIZZA with Buffalo Mozzarella Cheese",
+		"price": 400,
+		"category": "pizza"
+	},
+	"2208": {
+		"name": "PIZZA with Sausage",
+		"price": 400,
+		"category": "pizza"
+	},
+	"2206": {
+		"name": "PIZZA with Corn",
+		"price": 400,
+		"category": "pizza"
+	},
+	"2204": {
+		"name": "PIZZA with Mushroom & Vegetable Salsa",
+		"price": 400,
+		"category": "pizza"
+	},
+	"4106": {
+		"name": "Grilled Potato (Kid's Menu)",
+		"price": 100,
+		"category": "kid's menu"
+	},
+	"4105": {
+		"name": "Fried Shrimp (Kid's Menu)",
+		"price": 150,
+		"category": "kid's menu"
+	},
+	"2101": {
+		"name": "Doria with Meat Sauce",
+		"price": 300,
+		"category": "doria & gratin"
+	},
+	"2103": {
+		"name": "Doria with Meat Sauce & Egg",
+		"price": 350,
+		"category": "doria & gratin"
+	},
+	"2108": {
+		"name": "Doria with Meat Sauce & Cheese",
+		"price": 350,
+		"category": "doria & gratin"
+	},
+	"2106": {
+		"name": "Doria with 'TARAKO' Sauce & Shrimp",
+		"price": 400,
+		"category": "doria & gratin"
+	},
+	"2109": {
+		"name": "Gratin with shrimp & 'TARAKO' Sauce",
+		"price": 430,
+		"category": "doria & gratin"
+	},
+	"2320": {
+		"name": "Spaghetti 'TARAKO' Sauce with Shrimp",
+		"price": 540,
+		"category": "pasta"
+	},
+	"2321": {
+		"name": "Spaghetti Mushroom Cream with Spinach",
+		"price": 600,
+		"category": "pasta"
+	},
+	"2328": {
+		"name": "Spaghetti 'NERO DI SEPPIA'",
+		"price": 500,
+		"category": "pasta"
+	},
+	"2310": {
+		"name": "Spaghetti 'VONGOLE'",
+		"price": 500,
+		"category": "pasta"
+	},
+	"2305": {
+		"name": "Spaghetti 'CARBONARA'",
+		"price": 500,
+		"category": "pasta"
+	},
+	"2306": {
+		"name": "Spaghetti with Meat Sauce",
+		"price": 400,
+		"category": "pasta"
+	},
+	"2316": {
+		"name": "Spaghetti with Meat Sauce & Egg",
+		"price": 450,
+		"category": "pasta"
+	},
+	"2301": {
+		"name": "Spaghetti 'TARAKO' Sauce",
+		"price": 400,
+		"category": "pasta"
+	},
+	"2304": {
+		"name": "Spaghetti with Tomato & Pancetta",
+		"price": 400,
+		"category": "pasta"
+	},
+	"2303": {
+		"name": "Spaghetti 'PEPERONCINO'",
+		"price": 300,
+		"category": "pasta"
+	},
+	"2317": {
+		"name": "Spaghetti 'PEPERONCINO' with Egg",
+		"price": 350,
+		"category": "pasta"
+	},
+	"2325": {
+		"name": "Penne 'ARRABBIATA'",
+		"price": 430,
+		"category": "pasta"
+	},
+	"4307": {
+		"name": "Gran Moravia Cheese",
+		"price": 100,
+		"category": "topping"
+	},
+	"4301": {
+		"name": "Half Boiled Egg",
+		"price": 50,
+		"category": "topping"
+	},
+	"2406": {
+		"name": "Hamburg Steak",
+		"price": 400,
+		"category": "hamburg"
+	},
+	"2407": {
+		"name": "Hamburg Steak with Vegetable Salsa",
+		"price": 500,
+		"category": "hamburg"
+	},
+	"2403": {
+		"name": "Hamburg Steak with Cheese & Tomato Sauce",
+		"price": 500,
+		"category": "hamburg"
+	},
+	"2418": {
+		"name": "Hamburg Steak with Spicy Chicken & Fried Shrimp",
+		"price": 650,
+		"category": "hamburg"
+	},
+	"3101": {
+		"name": "Rice",
+		"price": 150,
+		"category": "side menu"
+	},
+	"3102": {
+		"name": "Large Rice",
+		"price": 200,
+		"category": "side menu"
+	},
+	"3103": {
+		"name": "Small Rice",
+		"price": 100,
+		"category": "side menu"
+	},
+	"2402": {
+		"name": "Grilled Chicken with Vegetable Salsa",
+		"price": 500,
+		"category": "chicken"
+	},
+	"2404": {
+		"name": "Grilled Chicken with Cheese & Tomato Sauce",
+		"price": 500,
+		"category": "chicken"
+	},
+	"2413": {
+		"name": "Grilled Lamb & Vegetables",
+		"price": 870,
+		"category": "lamb"
+	},
+	"4304": {
+		"name": "Vegetable Salsa",
+		"price": 100,
+		"category": "topping"
+	},
+	"3206": {
+		"name": "Italian Custard Pudding",
+		"price": 250,
+		"category": "dessert"
+	},
+	"3906": {
+		"name": "Italian Custard Pudding (later)",
+		"price": 250,
+		"category": "dessert"
+	},
+	"3201": {
+		"name": "Tiramisu",
+		"price": 300,
+		"category": "dessert"
+	},
+	"3901": {
+		"name": "Tiramisu (later)",
+		"price": 300,
+		"category": "dessert"
+	},
+	"3212": {
+		"name": "Tiramisu & Italian Custard Pudding",
+		"price": 500,
+		"category": "dessert"
+	},
+	"3912": {
+		"name": "Tiramisu & Italian Custard Pudding",
+		"price": 500,
+		"category": "dessert"
+	},
+	"3205": {
+		"name": "Italian Gelato",
+		"price": 250,
+		"category": "dessert"
+	},
+	"3905": {
+		"name": "Italian Gelato",
+		"price": 250,
+		"category": "dessert"
+	},
+	"3213": {
+		"name": "Truffle Ice Cream",
+		"price": 350,
+		"category": "dessert"
+	},
+	"3913": {
+		"name": "Truffle Ice Cream",
+		"price": 350,
+		"category": "dessert"
+	},
+	"3207": {
+		"name": "Chocolate Cake",
+		"price": 300,
+		"category": "dessert"
+	},
+	"3907": {
+		"name": "Chocolate Cake",
+		"price": 300,
+		"category": "dessert"
+	},
+	"3215": {
+		"name": "Coffee Jelly with Italian Gelato",
+		"price": 350,
+		"category": "dessert"
+	},
+	"3915": {
+		"name": "Coffee Jelly with Italian Gelato",
+		"price": 350,
+		"category": "dessert"
+	},
+	"3204": {
+		"name": "Petit Focaccia with Cinnamon Sugar & Italian Gelato",
+		"price": 450,
+		"category": "dessert"
+	},
+	"3904": {
+		"name": "Petit Focaccia with Cinnamon Sugar & Italian Gelato",
+		"price": 450,
+		"category": "dessert"
+	},
+	"3306": {
+		"name": "grappa ( 40%-alcohol / 30ml )",
+		"price": 300,
+		"category": "alcohol"
+	}
+}
